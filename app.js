8eb0e7fdc412bd3c88d9798d8947f500d6d52660
Stuart Huggins <stuhuggins@gmail.com>
2021-11-01 20:47:21 +0000

    end of template section

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+// const expressHbs = require('express-handlebars');
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const rootDir = require('./util/path');
 
 const app = express();
 
-app.set('view engine', 'pug');
+// app.engine('hbs', expressHbs({ layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs' }));
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,7 +20,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
     // res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
-    res.status(404).render('404');
+    res.status(404).render('404', { title: 'Page Not Found', path: '/not-found' });
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,15 @@ const adminData = require('./admin');
 const router = Router();
 
 router.get('/', (req, res, next) => {
-    res.render('shop', { products: adminData.products, title: 'Shop', path: '/' });
+    res.render('shop',
+        {
+            products: adminData.products,
+            title: 'Shop',
+            path: '/',
+            hasProducts: adminData.products.length > 0,
+            activeShop: true,
+            productCSS: true
+        });
 });
 
 module.exports = router;
