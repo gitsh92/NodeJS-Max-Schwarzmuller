ff0444581845615dfcc830783499918e3699b31e
Stuart Huggins <stuhuggins@gmail.com>
2021-11-16 11:46:20 +0000

    use dotenv

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const mongoose = require('mongoose');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 const rootDir = require('./util/path');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -17,34 +20,37 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, 'public')));
 
 app.use((req, res, next) => {
-    User.findById('618563eff76c716e9231696c')
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(console.log);
+  User.findById('618563eff76c716e9231696c')
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch(console.log);
 });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://stu:j9wcshEiKzKWyhJ4@cluster0.ukzuz.mongodb.net/shop?retryWrites=true&w=majority')
-    .then(result => {
-        User.findOne()
-            .then(user => {
-                if (!user) {
-                    const user = new User({
-                        name: 'Stuart',
-                        email: 'stuhuggins@gmail.com',
-                        cart: {
-                            items: []
-                        }
-                    });
-                    user.save();
-                }
-            });
-        app.listen(3000);
-    })
-    .catch(console.log);
+const connStr = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.ukzuz.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(connStr)
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: 'Stuart',
+          email: 'stuhuggins@gmail.com',
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    app.listen(process.env.APP_PORT || 3000);
+  })
+  .catch(console.log);
--- controllers/shop.js
+++ controllers/shop.js
@@ -2,118 +2,119 @@ const Product = require('../models/product');
 const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
-    Product.find()
-        .then(products => {
-            res.render('shop/index',
-                {
-                    products,
-                    title: 'All Products',
-                    path: '/products'
-                }
-            );
-        })
-        .catch(console.log);
+  Product.find()
+    .then((products) => {
+      res.render('shop/index', {
+        products,
+        title: 'All Products',
+        path: '/products',
+      });
+    })
+    .catch(console.log);
 };
 
 exports.getProduct = (req, res, next) => {
-    const { productId } = req.params;
-    Product.findById(productId)
-        .then(product => {
-            if (!product) {
-                return res.status(404).render('404', { title: 'Page Not Found', path: '/not-found' });
-            }
-            res.render('shop/product-detail', { title: product.title, path: '/products', product });
-        })
-        .catch(console.log);
-}
+  const { productId } = req.params;
+  Product.findById(productId)
+    .then((product) => {
+      if (!product) {
+        return res
+          .status(404)
+          .render('404', { title: 'Page Not Found', path: '/not-found' });
+      }
+      res.render('shop/product-detail', {
+        title: product.title,
+        path: '/products',
+        product,
+      });
+    })
+    .catch(console.log);
+};
 
 exports.getIndex = (req, res, next) => {
-    Product.find()
-        .then(products => {
-            res.render('shop/index',
-                {
-                    products,
-                    title: 'Shop',
-                    path: '/'
-                }
-            );
-        })
-        .catch(console.log);
+  Product.find()
+    .then((products) => {
+      res.render('shop/index', {
+        products,
+        title: 'Shop',
+        path: '/',
+      });
+    })
+    .catch(console.log);
 };
 
 exports.getCart = (req, res, next) => {
-    req.user
-        .populate('cart.items.productId')
-        .then(user => {
-            res.render('shop/cart', {
-                title: 'Your Cart',
-                path: '/cart',
-                products: user.cart.items 
-            });
-        })
-        .catch(console.log);
+  req.user
+    .populate('cart.items.productId')
+    .then((user) => {
+      res.render('shop/cart', {
+        title: 'Your Cart',
+        path: '/cart',
+        products: user.cart.items,
+      });
+    })
+    .catch(console.log);
 };
 
 exports.postCart = (req, res, next) => {
-    let { productId } = req.body;
-    Product.findById(productId)
-        .then(product => {
-            return req.user.addToCart(product);
-        })
-        .then(() => {
-            res.redirect('/cart');
-        })
-        .catch(console.log);
+  let { productId } = req.body;
+  Product.findById(productId)
+    .then((product) => {
+      return req.user.addToCart(product);
+    })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch(console.log);
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
-    const { productId } = req.body;
-    req.user
-        .removeFromCart(productId)
-        .then(result => {
-            res.redirect('/cart');
-        })
-        .catch(console.log);
+  const { productId } = req.body;
+  req.user
+    .removeFromCart(productId)
+    .then((result) => {
+      res.redirect('/cart');
+    })
+    .catch(console.log);
 };
 
 exports.getOrders = (req, res, next) => {
-    Order.find({ 'user.userId': req.user._id })
-        .then(orders => {
-            res.render('shop/orders', {
-                title: 'Your Orders',
-                path: '/orders',
-                orders
-            });
-        })
-        .catch(console.log);
-        
+  Order.find({ 'user.userId': req.user._id })
+    .then((orders) => {
+      res.render('shop/orders', {
+        title: 'Your Orders',
+        path: '/orders',
+        orders,
+      });
+    })
+    .catch(console.log);
 };
 
 exports.postOrder = (req, res, next) => {
-    req.user
-        .populate('cart.items.productId')
-        .then(user => {
-            const products = user.cart.items.map(i => {
-                return {
-                    quantity: i.quantity,
-                    product: { ...i.productId._doc }
-                };
-            });
+  req.user
+    .populate('cart.items.productId')
+    .then((user) => {
+      const products = user.cart.items.map((i) => {
+        return {
+          quantity: i.quantity,
+          product: { ...i.productId._doc },
+        };
+      });
 
-            const order = new Order({
-                user: {
-                    name: req.user.name,
-                    userId: req.user
-                },
-                products
-            });
-            return order.save();
-        })
-        .then(result => {
-            return req.user.clearCart();
-        })
-        .then(result => {
-            res.redirect('/orders');
-        })
-        .catch(console.log);
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user,
+        },
+        products,
+      });
+      return order.save();
+    })
+    .then((result) => {
+      return req.user.clearCart();
+    })
+    .then((result) => {
+      res.redirect('/orders');
+    })
+    .catch(console.log);
 };
